7f613d6fa16ed156e8e9a31dcc990c1920c2abc3
alzbetina <alzbeta.budirska@gmail.com>
2024-10-13 16:30:24 +0200

    Update kinomaniak_html.py

--- kinomaniak_html.py
+++ kinomaniak_html.py
@@ -1,33 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Kinomaniak:
+    def html_scrape(url: str):
+        search_page = requests.get(url)
+        soup = BeautifulSoup(search_page.content, 'html.parser')
 
-start_year = 2010
-url_part1 = "https://kinomaniak.cz/hledej/rozsirene/0/99/"
-url_part2 = "/0/0/0"
+        information_table = soup.find("table", class_="table table-hover table-bordered bg-white")
+        film_url = []
 
-def html_scrape(url: str):
-    search_page = requests.get(url)
-    soup = BeautifulSoup(search_page.content, 'html.parser')
+        for link in information_table.find_all('a'):
+            film_url.append(f'{"https://kinomaniak.cz"}{link.get("href")}')
+        return film_url
 
-    information_table = soup.find("table", class_="table table-hover table-bordered bg-white")
-    film_url = []
+    def films_urls(start_year, url_part1, url_part2):
+        all_films = []
 
-    for link in information_table.find_all('a'):
-        film_url.append(f'{"https://kinomaniak.cz"}{link.get("href")}')
-    return film_url
-
-def films_urls(start_year, url_part1, url_part2):
-    all_films = []
-
-    for year in range(2010, 2025):
-        url_year = url_part1 + str(start_year) + url_part2
-        year_list = html_scrape(url_year)
-        all_films.extend(year_list)
-        start_year += 1
-    return all_films
-
-
-data = films_urls(start_year, url_part1, url_part2)
-print(data)
-
+        for year in range(2010, 2025):
+            url_year = url_part1 + str(start_year) + url_part2
+            year_list = html_scrape(url_year)
+            all_films.extend(year_list)
+            start_year += 1
+        return all_films
