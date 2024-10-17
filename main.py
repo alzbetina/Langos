431c238f90f646528c6ed8ef956752cf2f6660a6
Efca Mazankova <evis.mazankova@seznam.cz>
2024-10-17 20:20:06 +0200

    main generuje Json soubor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kinomaniak import Kinomaniak
+import json
 
 scraper_kinomaniak = Kinomaniak()
 scraper_kinomaniak.scrape(2024)
-print(scraper_kinomaniak.all_films)
+
+with open("Kinomaniak.json", mode="w", encoding="utf-8") as kinomaniak_file:
+    json.dump(scraper_kinomaniak.all_films, kinomaniak_file, ensure_ascii=False, indent=4)
