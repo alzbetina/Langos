2de75bedebe5716f254b8ebd55781b2d3fca6444
alzbetina <alzbeta.budirska@gmail.com>
2024-10-19 13:54:16 +0200

    přidána větev CSFD

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from kinomaniak import Kinomaniak
+from CSFD import CSFD
 import json
 
 scraper_kinomaniak = Kinomaniak()
 scraper_kinomaniak.scrape(2024)
 
+scraper_CSFD = CSFD()
+scraper_CSFD.scrape(1)
+
 with open("Kinomaniak.json", mode="w", encoding="utf-8") as kinomaniak_file:
     json.dump(scraper_kinomaniak.all_films, kinomaniak_file, ensure_ascii=False, indent=4)
+
+with open("CSFD.json", mode="w", encoding="utf-8") as CSFD_file:
+    json.dump(scraper_CSFD.all_films, CSFD_file, ensure_ascii=False, indent=4)
