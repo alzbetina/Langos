652999f23c8f3fe15c5daac7aafaa3485b18c60c
alzbetina <alzbeta.budirska@gmail.com>
2024-10-19 12:14:27 +0200

    final scrape

--- CSFD.py
+++ CSFD.py
@@ -2,18 +2,22 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-page_start = 1
-
 
 class CSFD:
+    def scrape (self, page_start):
+        self.scrape_film_CSFD(page_start)
+        self.all_films = []
+        for film_url in self.all_films_url:
+            film = self.scrape_film(film_url)
+            self.all_films.append(film)
+
     def scrape_film_CSFD(self, page_start):
         self.all_films_url = []
 
         for page in range(page_start, 8):
             url_page = f'https://www.csfd.cz/podrobne-vyhledavani/?page={page}&searchParams=rlW0rKOyVwcoZS0fVzqyoaWyVwc7VwRvBygqYPVlVwcoKFjvZlV6JmxfZGIqYPV0VwcoZljkZljkAPjlZljkZvjmZS0fVaE5pTHvBwA9YPWipzyanJ4vBafvZFV6J10fVwVvByfkKFjvZlV6J10fVwDvByf2APjkAmWqYPW0rKOyVwblsFjvrJIupy9zpz9gVwblZQRjYPW5MJSlK3EiVwblZQV0YPWlLKEcozqsMaWioFV6oaIfoPjvpzS0nJ5aK3EiVwchqJkfYPW0LJpvBygqYPWuL3EipvV6J10fVzEcpzIwqT9lVwcoKFjvL29gpT9mMKVvBygqYPWmL3WyMJ53pzy0MKVvBygqYPWuqKEbo3VvBygqYPWwnJ5yoJS0o2qlLKObMKVvBygqYPWjpz9xqJA0nJ9hVwcoKFjvL2SmqTyhMlV6J10fVzIxnKDvBygqYPWmo3IhMPV6J10fVaAwMJ5iM3WupTu5VwcoKFjvoJSmnlV6J10fVzAip3E1oJImVwcoKFjvL29hMTy0nJ9hplV6J119'
             page_list = self.scrape_films_page(url_page)
-            self.all_films_url.extend(page_list)
-        
+            self.all_films_url.extend(page_list)        
     
     def scrape_films_page(self, url: str):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
@@ -71,8 +75,7 @@ class CSFD:
         rating = inforating.find("div", class_="film-rating-average").text.strip()
         rates_parent = inforating.find(string=re.compile("Hodnocení"))
         rates = rates_parent.parent.find_next("span").text.replace("(","").replace(")","").replace("\xa0","")
-    
-    
+        
         #dictionary keys
         film_keys = ["Movie Title", "Rating", "Number of Rates", "Genre", "Origin", "Actors"]
         film_values = [title, rating, rates, genre, origin_clean, actors_list]
